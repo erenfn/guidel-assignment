77d24eea86db4239034ddab3a60566eeb82791fa
erenfn <erenfindik7@gmail.com>
2024-11-06 16:12:04 +0300

    map integration

---
+++ lib/MapScreen/map_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  late GoogleMapController mapController;
+
+  final LatLng _initialPosition = const LatLng(37.7749, -122.4194); // Example coordinates (San Francisco)
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Full Screen Google Map'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _initialPosition,
+          zoom: 12,
+        ),
+        mapType: MapType.normal,
+        myLocationEnabled: true,
+        zoomControlsEnabled: false,
+      ),
+    );
+  }
+}
